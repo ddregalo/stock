be9af1d733b56bea3eb0b10330e983de88201d6f
danielcampos <ddregalo@gmail.com>
2018-03-31 02:07:01 +0100

    Add artwork stock inventory schema to asiModel

---
+++ src/models/asiModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ArtworkSchema = new Schema({
+  artist: {
+    type: String,
+    required: 'Enter artist name.'
+  },
+  artworkImage: {
+    data: Buffer,
+    contentType: String
+  },
+  artworkTitle: {
+    type: String,
+    required: 'Enter the artwork title.'
+  },
+  artworkYear: {
+    type: Number,
+    required: 'Enter the year the artwork was created.'
+  },
+  artworkCollection: {
+    type: String,
+  },
+  artworkMedium: {
+    type: String,
+    required: 'Enter the artwork medium.'
+  },
+  artworkEdition: {
+    type: String,
+    required: 'Enter the artwork edition or unique.'
+  },
+  artworkSigned: {
+    type: String,
+    required: 'Enter yes or no if the artwork is signed.'
+  },
+  artworkPrice: {
+    type: Number,
+    required: 'Enter artwork price or 0 for not for sale.'
+  },
+  create_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = ArtworkSchema;
